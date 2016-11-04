0de3d38eafbdc5fcb62966ab68a0b57ca1e0de7f
Tomy Bezenger <tbezenger@gmail.com>
2016-11-04 18:22:11 +0100

    maj rapport + relation bdd pour la connexion

--- ClientPlanificateur/src/main/java/fr/univtln/projuml/clt/Models/ConnectionModel.java
+++ ClientPlanificateur/src/main/java/fr/univtln/projuml/clt/Models/ConnectionModel.java
@@ -1,7 +1,15 @@
 package fr.univtln.projuml.clt.Models;
 
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 import fr.univtln.projuml.clt.Users.CUser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -11,9 +19,15 @@ import java.util.Observer;
  */
 public class ConnectionModel extends Observable implements Observer {
     private static ConnectionModel connectionModel = new ConnectionModel();
-    private  ConnectionModel(){}
+    private  ConnectionModel(){
+        cc.getClasses().add(JacksonJsonProvider.class);
+        Client c = Client.create(cc);
+        webResource = c.resource("http://176.157.85.69:9999/");;
+    }
     public static ConnectionModel getInstance(){return connectionModel;}
 
+    private ClientConfig cc = new DefaultClientConfig();
+    WebResource webResource;
     private CUser currentUser;
 
     public void update(Observable o, Object arg) {
@@ -27,14 +41,24 @@ public class ConnectionModel extends Observable implements Observer {
 
     public boolean createAccount(String mail, String pw){
         int hashedPw = hash(pw);
+        // TODO :  factoriser la webressource
+
         CUser newUser = new CUser(mail, hashedPw);
-        // TODO : envoyer au serveur pour persistance
+        webResource.path("users").type(MediaType.APPLICATION_JSON).post(newUser);
         currentUser = newUser;
         return true;
     }
 
     public boolean userConnection(String mail, String pw){
         // TODO : verifier dans la bdd que l'utilisateur est correct et le definir comme "currentUser" (/!\ au hash)
+        ObjectMapper om = new ObjectMapper();
+
+        String u = webResource.path("users/mail/"+mail).type(MediaType.APPLICATION_JSON).get(String.class);
+        try {
+            currentUser = om.readValue(u, CUser.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return true;
     }
 
